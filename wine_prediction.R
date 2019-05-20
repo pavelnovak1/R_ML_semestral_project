b89e43478c7be5cbc9ca626c6d97f81216641ee4
Pavel Novak <pavel.novak98@email.cz>
2019-05-20 06:55:36 +0200

    Model trained by train function, more preprocessing, students performance dataset added

--- wine_prediction.R
+++ wine_prediction.R
@@ -1,5 +1,7 @@
 library(dplyr)
 library(RWeka)
+library(caret)
+library(rpart.plot)
 ###### loading data #######
 
 wine.all <- read.csv("winequalityN.csv")
@@ -12,7 +14,6 @@ summary(wine.all)
 ##### preprocessing #####
 
 wine.all <- wine.all[sample(nrow(wine.all)), ]
-wine.all$quality <- as.factor(wine.all$quality)
 
 wine.all[is.na(wine.all$fixed.acidity), "fixed.acidity"] <- mean(wine.all$fixed.acidity, na.rm = T)
 wine.all[is.na(wine.all$volatile.acidity), "volatile.acidity"] <- mean(wine.all$volatile.acidity, na.rm = T)
@@ -22,34 +23,43 @@ wine.all[is.na(wine.all$chlorides), "chlorides"] <- mean(wine.all$chlorides, na.
 wine.all[is.na(wine.all$pH), "pH"] <- mean(wine.all$pH, na.rm = T)
 wine.all[is.na(wine.all$sulphates), "sulphates"] <- mean(wine.all$sulphates, na.rm = T)
 
+wine.all[(wine.all$quality == 3), "quality"] <- 4
+wine.all[(wine.all$quality == 9), "quality"] <- 8
+wine.all$quality <- as.factor(wine.all$quality)
+plot(wine.all$quality)
+
+
 summary(wine.all)
 plot(wine.all$type)
 plot(wine.all$quality)
 
-wine.train <- wine.all[1:3250, ]
-wine.test <- wine.all[3251:6497, ]
+wine.train <- wine.all[1:2100, ]
+wine.test <- wine.all[2101:6497, ]
 
 
 ###### model ######
 
-model <- J48(quality ~ ., data = wine.train, control = Weka_control(R = F, M = 200, A = F))
-prediction <- predict(model, wine.test)
+#model.wine <- J48(quality ~ ., data = wine.train, control = Weka_control(R = T, M = 200))
+model.wine <- train(quality ~ ., data = wine.train, method = "J48", preProcess = c("center", "scale"), trControl = trainControl("cv"))
+###### evaluation ######
 
-references <- wine.test$quality
+prediction.wine <- predict(model.wine, wine.test)
 
-confmat <- table(prediction, references)
-confmat
+references.wine <- wine.test$quality
 
-accuracy2 <- confmat[1:1] + confmat[1,2]
-for(i in 2:6){
+confmat.wine <- table(prediction.wine, references.wine)
+confmat.wine
+
+accuracy.wine.2 <- confmat.wine[1:1] + confmat.wine[1,2]
+for(i in 2:4){
   for(j in (i-1):(i+1)){
-    accuracy2 <- accuracy2 + confmat[i,j]
+    accuracy.wine.2 <- accuracy.wine.2 + confmat.wine[i,j]
   }
 }
-accuracy2 <- accuracy2 + confmat[7,6] + confmat[7,7]
-accuracy2 <- accuracy2 / sum(confmat)
-accuracy2
+accuracy.wine.2 <- accuracy.wine.2 + confmat.wine[5,4] + confmat.wine[5,5]
+accuracy.wine.2 <- accuracy.wine.2 / sum(confmat.wine)
+accuracy.wine.2
 
-accuracy <- sum(diag(confmat)) / sum(confmat)
-accuracy
+accuracy.wine <- sum(diag(confmat.wine)) / sum(confmat.wine)
+accuracy.wine
 
